6ac1f05a0a120e350074c6ce828b1ebf79385e51
MOHSEN <baxdev1@gmail.com>
2026-09-22 00:57:28 +0330

    feat(accounts): add logout, register to project

--- accounts/views.py
+++ accounts/views.py
@@ -24,3 +24,31 @@ def login_view(request):
         return render(request, "accounts/login.html", context)
     else:
         return redirect("/")
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect("/")
+
+
+def register_view(request):
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get("username")
+                password = form.cleaned_data.get("password1")
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                return redirect("/")
+            else:
+                return render(request, "accounts/register.html", {"form": form})
+
+        form = UserCreationForm()
+        context = {"form": form}
+        return render(request, "accounts/register.html", context)
+    else:
+        return redirect("/")
