f18e919b9eab3fa663e487ba91e340414d737dce
MOHSEN <baxdev1@gmail.com>
2026-09-22 01:31:42 +0330

    pref(todo): some opimization on requested user

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="task_list"),
+    path("update/<str:pk>/", views.updateTask, name="update_task"),
+    path("complete/<str:pk>/", views.completeTask, name="complete_task"),
+    path("delete/<str:pk>/", views.deleteTask, name="delete_task"),
 ]
