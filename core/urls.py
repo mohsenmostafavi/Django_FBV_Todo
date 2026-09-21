99adb17c1929ba1b402438c1c8cbf5fc4f150be5
MOHSEN <baxdev1@gmail.com>
2026-09-22 01:24:06 +0330

    feat(todo): creat and list task

--- core/urls.py
+++ core/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
-    path("todo/", include("todo.urls")),
+    path("", include("todo.urls")),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def index(request):
+    tasks = Task.objects.filter(user=request.user)
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user  
+            task.save()
+            return redirect("/")
+    else:
+        form = TaskForm()
+
+    context = {"tasks": tasks, "form": form}
+    return render(request, "tasks/list_task.html", context)
